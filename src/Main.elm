3a1e97a98f9d73b5138c619a616541b443059b14
Leandro Bighetti <lbighetti@gmail.com>
2018-06-20 13:25:04 +0200

    Prototype of Layout
    * Started from Boostrap basic starter template,
    removed unnecessary elements
    
    * Fixed link to Boostrap v4.0 CDN
    
    * Added .css from starter template

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,33 @@
-import Html exposing (text)
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
 
 main =
-  text "Welcome dear friend! Focus Potion is under construction!"
+    div []
+        [ nav [ class "navbar navbar-expand-md navbar-dark bg-dark fixed-top" ]
+            [ a [ class "navbar-brand", href "#" ]
+                [ text "Focus Potion" ]
+            ]
+        , div [ class "container" ]
+            [ div [ class "starter-template" ]
+                [ div [ class "row" ]
+                    [ div [ class "col-md-12" ]
+                        [ h1 []
+                            [ text "25:00" ]
+                        , p [ class "lead" ]
+                            [ text "Focus!" ]
+                        ]
+                    ]
+                , div [ class "row" ]
+                    [ div [ class "col-md-4" ]
+                        [ div [ class "btn btn-primary btn-block" ] [ text "focus" ] ]
+                    , div [ class "col-md-4" ]
+                        [ div [ class "btn btn-secondary btn-block" ] [ text "small break" ] ]
+                    , div [ class "col-md-4" ]
+                        [ div [ class "btn btn-secondary btn-block" ] [ text "big break" ] ]
+                    ]
+                ]
+            ]
+        ]
