b2541e9f633e09128ae8e691c35bee49959be4d5
Leandro Bighetti <lbighetti@gmail.com>
2018-06-27 14:43:05 +0200

    Brunch setup with Elm and Autoreload
    * Basic brunch resulted in one additional .js file
    which was included in index.html
    
    * Moved files around and created directories for proper structure
    
    * Brunch watch now possible and triggers browser autoreload

---
+++ brunch-config.js
@@ -0,0 +1,25 @@
+// brunch-config.js
+module.exports = {
+  files: {
+    javascripts: {joinTo: 'js/app.js'},
+    stylesheets: {joinTo: 'css/app.css'}
+  },
+
+  plugins: {
+    elmBrunch: {
+      mainModules: ['app/elm/Main.elm'],
+      outputFolder: 'public/js',
+      /* '--debug' parameter activates Elm 0.18 history debugger */
+      makeParameters: '--debug'
+    },
+  },
+  overrides: {
+    production: {
+      plugins: {
+        elmBrunch: {
+          makeParameters: []
+        }
+      }
+    }
+  }
+}
