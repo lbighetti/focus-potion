1590405568ff7ebbe8222681aa2b0d53a947a7b8
Leandro Bighetti <lbighetti@gmail.com>
2018-07-30 17:36:28 +0200

    Small refactor to improve readability

--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -99,108 +99,122 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ nav [ class "navbar navbar-expand-md navbar-dark bg-dark fixed-top" ]
-            [ a [ class "navbar-brand", href "#" ]
-                [ text "Focus Potion" ]
-            ]
+        [ navbar
         , div [ class "container" ]
             [ div [ class "starter-template" ]
-                [ div [ class "row" ]
-                    [ div [ class "col-md-12" ]
-                        [ h1 [ class "display-4" ]
-                            [ text (formatTime model.counter) ]
-                        ]
-                    ]
-                , div [ class "row" ]
-                    [ div [ class "col-md-12" ]
-                        [ p [ class "lead" ]
-                            [ text
-                                (case model.period of
-                                    Focusing ->
-                                        if model.counter >= 0 then
-                                            "Focus!"
-                                        else
-                                            "Time to take a break!"
+                (bodySection model)
+            ]
+        ]
 
-                                    InSmallBreak ->
-                                        if model.counter >= 0 then
-                                            "Catch your breath."
-                                        else
-                                            "Time to Focus again!"
 
-                                    InBigBreak ->
-                                        if model.counter >= 0 then
-                                            "Relax and enjoy!"
-                                        else
-                                            "Time to Focus again!"
 
-                                    Stopped ->
-                                        "Ready to roll?"
-                                )
-                            ]
-                        ]
-                    ]
-                , div [ class "row justify-content-center mt-3" ]
-                    [ div [ class "col-md-12" ]
-                        [ div [ class "progress" ]
-                            [ div
-                                [ classList
-                                    [ ( "progress-bar progress-bar-striped progress-bar-animated", True )
-                                    , ( "bg-primary", model.period == Focusing )
-                                    , ( "bg-secondary", model.period == InBigBreak || model.period == InSmallBreak )
-                                    , ( "bg-danger", model.period == Stopped )
-                                    ]
-                                , attribute "style" (progressPercentageString model.counter model.counterMax)
-                                ]
-                                []
-                            ]
-                        ]
-                    ]
-                , div [ class "row mt-5" ]
-                    [ div [ class "col-md-3 mt-1" ]
-                        [ button [ class "btn btn-primary btn-block", onClick Focus ]
-                            [ i [ class "fas fa-hourglass-half fa-lg" ]
-                                []
-                            ]
-                        ]
-                    , div [ class "col-md-3 mt-1" ]
-                        [ button [ class "btn btn-secondary btn-block", onClick SmallBreak ]
-                            [ i [ class "fas fa-coffee fa-lg" ]
-                                []
-                            ]
-                        ]
-                    , div [ class "col-md-3 mt-1" ]
-                        [ button [ class "btn btn-secondary btn-block", onClick BigBreak ]
-                            [ i [ class "fas fa-coffee fa-lg" ]
-                                []
-                            , i [ class "fas fa-coffee fa-lg" ]
-                                []
-                            ]
-                        ]
-                    , div [ class "col-md-3 mt-1" ]
-                        [ button [ class "btn btn-outline-danger btn-block", onClick Stop ]
-                            [ i [ class "fas fa-ban fa-lg" ] []
-                            ]
-                        ]
-                    ]
-                , div [ class "row justify-content-center mt-5" ]
-                    [ div [ class "col-md-6" ]
-                        [ div
-                            [ class "input-group input-group-lg" ]
-                            [ div [ class "input-group-prepend" ]
-                                [ span [ class "input-group-text" ]
-                                    [ i [ class "fas fa-tag" ]
-                                        []
-                                    ]
-                                ]
-                            , input [ class "form-control" ]
-                                []
-                            ]
-                        ]
-                    ]
+-- View Pieces
+
+
+navbar : Html msg
+navbar =
+    nav [ class "navbar navbar-expand-md navbar-dark bg-dark fixed-top" ]
+        [ a [ class "navbar-brand", href "#" ]
+            [ text "Focus Potion" ]
+        ]
+
+
+bodySection : Model -> List (Html Msg)
+bodySection model =
+    [ div [ class "row" ]
+        [ div [ class "col-md-12" ]
+            [ h1 [ class "display-4" ]
+                [ text (formatTime model.counter) ]
+            ]
+        ]
+    , div [ class "row" ]
+        [ div [ class "col-md-12" ]
+            [ p [ class "lead" ]
+                [ text
+                    (case model.period of
+                        Focusing ->
+                            if model.counter >= 0 then
+                                "Focus!"
+                            else
+                                "Time to take a break!"
+
+                        InSmallBreak ->
+                            if model.counter >= 0 then
+                                "Catch your breath."
+                            else
+                                "Time to Focus again!"
+
+                        InBigBreak ->
+                            if model.counter >= 0 then
+                                "Relax and enjoy!"
+                            else
+                                "Time to Focus again!"
+
+                        Stopped ->
+                            "Ready to roll?"
+                    )
                 ]
             ]
         ]
+    , div [ class "row justify-content-center mt-3" ]
+        [ div [ class "col-md-12" ]
+            [ div [ class "progress" ]
+                [ div
+                    [ classList
+                        [ ( "progress-bar progress-bar-striped progress-bar-animated", True )
+                        , ( "bg-primary", model.period == Focusing )
+                        , ( "bg-secondary", model.period == InBigBreak || model.period == InSmallBreak )
+                        , ( "bg-danger", model.period == Stopped )
+                        ]
+                    , attribute "style" (progressPercentageString model.counter model.counterMax)
+                    ]
+                    []
+                ]
+            ]
+        ]
+    , div [ class "row mt-5" ]
+        [ div [ class "col-md-3 mt-1" ]
+            [ button [ class "btn btn-primary btn-block", onClick Focus ]
+                [ i [ class "fas fa-hourglass-half fa-lg" ]
+                    []
+                ]
+            ]
+        , div [ class "col-md-3 mt-1" ]
+            [ button [ class "btn btn-secondary btn-block", onClick SmallBreak ]
+                [ i [ class "fas fa-coffee fa-lg" ]
+                    []
+                ]
+            ]
+        , div [ class "col-md-3 mt-1" ]
+            [ button [ class "btn btn-secondary btn-block", onClick BigBreak ]
+                [ i [ class "fas fa-coffee fa-lg" ]
+                    []
+                , i [ class "fas fa-coffee fa-lg" ]
+                    []
+                ]
+            ]
+        , div [ class "col-md-3 mt-1" ]
+            [ button [ class "btn btn-outline-danger btn-block", onClick Stop ]
+                [ i [ class "fas fa-ban fa-lg" ] []
+                ]
+            ]
+        ]
+    , div [ class "row justify-content-center mt-5" ]
+        [ div [ class "col-md-6" ]
+            [ div
+                [ class "input-group input-group-lg" ]
+                [ div [ class "input-group-prepend" ]
+                    [ span [ class "input-group-text" ]
+                        [ i [ class "fas fa-tag" ]
+                            []
+                        ]
+                    ]
+                , input [ class "form-control" ]
+                    []
+                ]
+            ]
+        ]
+    ]
 
 
 
